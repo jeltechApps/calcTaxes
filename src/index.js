f43c6c765e1a5a5bfb5568b2ec2a8bbb1c549cdc
Paul Kilmurray <paul@kilbot.com.au>
2018-04-04 18:35:54 +0700

    WIP

--- src/index.js
+++ src/index.js
@@ -1,10 +1,125 @@
 // @flow
+import _ from "lodash";
 
 /**
- * This function says hello.
- * @param name Some name to say hello for.
- * @returns The hello.
+ * This module calculates inclusive and exclusive taxes
  */
-const sayHello = (name: string = "Haz"): string => `Hello, ${name}!`;
 
-export default sayHello;
+type Rate = {
+  id: number,
+  rate: string,
+  compound: boolean
+};
+
+type Rates = Array<Rate>;
+
+type Tax = {
+  id: number,
+  total: number
+};
+
+type Taxes = {
+  taxes: Array<Tax>,
+  totalTax: number
+};
+
+/**
+ * calcExclusiveTax
+ * @param price
+ * @param rates
+ * @returns {number}
+ */
+const calcExclusiveTax = (price: number, rates: Rates): Taxes => {
+  let preCompoundTotalTax = 0;
+  const taxes = [];
+
+  _(rates).forEach(tax => {
+    if (!tax.compound) {
+      taxes.push({
+        id: tax.id,
+        total: price * (parseFloat(tax.rate) / 100)
+      });
+    }
+  });
+
+  preCompoundTotalTax = _(taxes).reduce((a, b) => a.total + b.total, 0);
+
+  _(rates).forEach(tax => {
+    if (tax.compound) {
+      taxes.push({
+        id: tax.id,
+        total: (price + preCompoundTotalTax) * (parseFloat(tax.rate) / 100)
+      });
+    }
+  });
+
+  const totalTax = _.sumBy(taxes, "total");
+
+  return { taxes, totalTax };
+};
+
+/**
+ * calcInclusiveTax
+ * @param price
+ * @param rates
+ * @returns {number}
+ */
+const calcInclusiveTax = (price: number, rates: Rates): Taxes => {
+  let regularTaxRates = 0;
+  let compoundTaxRates = 0;
+  let nonCompoundPrice = 0;
+  const taxes = [];
+
+  _(rates).forEach(tax => {
+    const rate = parseFloat(tax.rate) / 100;
+    if (tax.compound) {
+      compoundTaxRates += rate;
+    } else {
+      regularTaxRates += rate;
+    }
+  });
+
+  nonCompoundPrice = price / (1 + compoundTaxRates);
+
+  _(rates).forEach(tax => {
+    let theRate = parseFloat(tax.rate) / 100;
+    let thePrice = 0;
+
+    if (tax.compound) {
+      thePrice = price;
+      theRate /= 1 + compoundTaxRates;
+    } else {
+      thePrice = nonCompoundPrice;
+      theRate /= 1 + regularTaxRates;
+    }
+
+    taxes.push({
+      id: tax.id,
+      total: theRate * thePrice
+    });
+  });
+
+  const totalTax = _.sumBy(taxes, "total");
+
+  return { taxes, totalTax };
+};
+
+/**
+ * calcTaxes
+ * @param price
+ * @param rates
+ * @param priceIncludeTax
+ * @returns {number}
+ */
+const calcTaxes = (
+  price: number,
+  rates: Rates = [],
+  priceIncludeTax: boolean = false
+): Taxes =>
+  priceIncludeTax
+    ? calcInclusiveTax(price, rates)
+    : calcExclusiveTax(price, rates);
+
+export { calcExclusiveTax, calcInclusiveTax };
+
+export default calcTaxes;
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,42 @@
-import sayHello from "../src";
+import calcTaxes from "../src";
 
-describe("sayHello", () => {
-  it("returns hello", () => {
-    expect(sayHello()).toBe("Hello, Haz!");
-    expect(sayHello("foo")).toBe("Hello, foo!");
+describe("calcTaxes", () => {
+  it("returns 0 tax if no tax rates provided", () => {
+    expect(calcTaxes(9.99)).toEqual({
+      taxes: [],
+      totalTax: 0
+    });
+  });
+  it("returns exclusive tax by default", () => {
+    expect(calcTaxes(9.99, [{ id: 1, rate: "20.0000" }])).toEqual({
+      taxes: [{ id: 1, total: 1.998 }],
+      totalTax: 1.998
+    });
+  });
+  it("returns inclusive tax with flag", () => {
+    expect(calcTaxes(9.99, [{ id: 1, rate: "20.0000" }], true)).toEqual({
+      taxes: [{ id: 1, total: 1.665 }],
+      totalTax: 1.665
+    });
+  });
+  it("returns compound exclusive tax with compound flag", () => {
+    const rates = [
+      { id: 1, rate: "5.0000", compound: false },
+      { id: 2, rate: "8.5000", compound: true }
+    ];
+    expect(calcTaxes(100, rates)).toEqual({
+      taxes: [{ id: 1, total: 5 }, { id: 2, total: 8.925 }],
+      totalTax: 13.925
+    });
+  });
+  it("returns compound inclusive tax with compound flag", () => {
+    const rates = [
+      { id: 1, rate: "5.0000", compound: false },
+      { id: 2, rate: "8.5000", compound: true }
+    ];
+    expect(calcTaxes(100, rates)).toEqual({
+      taxes: [{ id: 1, total: 4.3889 }, { id: 2, total: 7.8341 }],
+      totalTax: 12.223
+    });
   });
 });
